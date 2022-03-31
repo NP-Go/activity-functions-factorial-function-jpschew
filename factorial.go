a5ae4fb8b7b84f8f2f13f31d2667459f2f327deb
jpschew <jps.chew@gmail.com>
2022-03-31 10:07:07 +0800

    added code to define factorial function

--- factorial.go
+++ factorial.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 //Declare factorial function here
+func factorial(num int) int {
+	if num <= 1 {
+		return 1
+	} else {
+		return num * factorial(num-1)
+	}
+}
 
 func main() {
 	//Insert Code here
+	var userInput int
+	fmt.Println("Please enter a number: ")
+	fmt.Scan(&userInput)
+	result := factorial(userInput)
+	fmt.Println(result)
 }
